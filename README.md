27e73879419e21025b383cd1d7e4390ea3241113
iamim2022 <procom7612@gmail.com>
2022-06-07 10:48:46 +0900

    renew repository

---
+++ README.md
@@ -0,0 +1,4 @@
+# Web programming
+
+- page : https://iamim2022.github.io/Web/
+- game : https://iamim2022.github.io/Web/game/index.html
---
+++ js/script.js
@@ -0,0 +1,10 @@
+//자바스크립트 코드를 여기에 짜면 된다.
+
+function greeting(){
+  console.log('Hello World');
+}
+
+function Lunch(){
+  console.log('Pizzaaaaaa');
+}
+
