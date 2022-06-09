00ced6567526361d41755dde98ac09d9a4dcd662
iamim2022 <procom7612@gmail.com>
2022-06-09 11:26:15 +0900

    modify

--- j/Word.java
+++ j/Word.java
@@ -1,21 +1,29 @@
 public class Word {
+	private String letters;
+
 	public static void main(String[] args) {
 		Word word = new Word("Hello World");
-		
-		for(int i = 0; i < word.letters.length(); i++) {
+
+		// for문으로 읽기
+		for (int i = 0; i < word.letters.length(); i++) {
 			System.out.println(word.isVowel(i));
 		}
 	}
-	
-	private String letters;
 
+//	생성자
+//	this.letters = new String letters;   가 축약된 것.
 	public Word(String letters) {
 		this.letters = letters.toLowerCase();
 	}
-	
+
 //	i번째 글자의 모음 여부  _ a,i,u,e,o
 	public boolean isVowel(int i) {
-		return letters.substring(i, i + 1).equals("a") || letters.substring(i, i + 1).equals("i")  || letters.substring(i, i + 1).equals("u")
-				 || letters.substring(i, i + 1).equals("e")  || letters.substring(i, i + 1).equals("o");
-	}	
+		String[] vowel = { "a", "i", "u", "e", "o" };
+		for (int j = 0; j < vowel.length; j++) {
+			if (letters.substring(i, i + 1).equals(vowel[j])) {
+				return true;
+			}
+		}
+		return false;
+	}
 }
